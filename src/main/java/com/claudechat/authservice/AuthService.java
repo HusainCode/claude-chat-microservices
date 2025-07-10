66e834c4d489d03022bff375f6badc021734705d
Husain Alshaikhahmed <59243432+HusainCode@users.noreply.github.com>
2025-07-09 21:51:23 -0500

    Start auth

--- src/main/java/com/claudechat/authservice/AuthService.java
+++ src/main/java/com/claudechat/authservice/AuthService.java
@@ -1,6 +1,16 @@
 package com.claudechat.authservice;
 
 public class AuthService {
+
+    public boolean register(String username, String password) {
+        // TODO: Implement user registration
+        return false;
+    }
+
+    public boolean login(String username, String password) {
+        // TODO: Implement login check
+        return false;
+    }
+
+
 }
-
-
