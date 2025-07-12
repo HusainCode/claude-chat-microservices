aa002908f430563e777271a5e5463660b430199c
Husain Alshaikhahmed <59243432+HusainCode@users.noreply.github.com>
2025-07-11 22:19:07 -0500

    Started chat

--- src/main/java/com/claudechat/claudeconnector/CloudConnector.java
+++ src/main/java/com/claudechat/claudeconnector/CloudConnector.java
@@ -1,6 +1,21 @@
 package com.claudechat.claudeconnector;
 
+import org.springframework.web.bind.annotation.*;
+import software.amazon.awssdk.services.bedrockruntime.BedrockRuntimeClient;
+import software.amazon.awssdk.services.bedrockruntime.model.InvokeModelRequest;
+import software.amazon.awssdk.services.bedrockruntime.model.InvokeModelResponse;
 
-
+@RestController
+@RequestMapping
 public class CloudConnector {
+
+    private final BedrockRuntimeClient bedrockClient = BedrockRuntimeClient.create();
+    public String chatWithBot(@RequestBody String prompt){
+        String modelID = ""; // pick modle
+        InvokeModelRequest request = InvokeModelRequest.builder()
+                .modelId(modelID)
+                .body("{\") // stopd here
+
+
+    }
 }
