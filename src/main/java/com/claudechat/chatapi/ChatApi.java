b6a08c86df5d8a7e5bc15bba1c63f21d30b91894
Husain Alshaikhahmed <59243432+HusainCode@users.noreply.github.com>
2025-07-16 23:20:29 -0500

    Update ChatApi.java

--- src/main/java/com/claudechat/chatapi/ChatApi.java
+++ src/main/java/com/claudechat/chatapi/ChatApi.java
@@ -1,7 +1,24 @@
 package com.claudechat.chatapi;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChatApi {
 
-    
-}
+    private List<String> messageLog = new ArrayList<>();
 
+    public void sendMessage(String user, String message) {
+        // TODO: implement message sending
+        String entry = String.format("[%s]: %s", user, message);
+        messageLog.add(entry);
+        System.out.println("Sent: " + entry);
+    }
+
+    public List<String> getMessageLog() {
+        return messageLog;
+    }
+
+    public int getMessageCount() {
+        return messageLog.size();
+    }
+}
